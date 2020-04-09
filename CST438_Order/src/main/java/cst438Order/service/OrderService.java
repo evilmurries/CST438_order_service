ddfe6afa817a15d659e496729d5bc8c96816327c
rices <rices@192.168.0.17>
2020-04-08 20:17:05 -0700

    Added Message to OrderService

--- CST438_Order/src/main/java/cst438Order/service/OrderService.java
+++ CST438_Order/src/main/java/cst438Order/service/OrderService.java
@@ -1,15 +1,35 @@
 package cst438Order.service;
 
+import java.util.List;
+
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import cst438Order.domain.Order;
+import cst438Order.domain.OrderRepository;
+
+
 @Service
 public class OrderService {
 	
 	
+	@Autowired
+	private OrderRepository orderRepository;
 	
+	public Order getOrder(String restaurantName) {
+		
+		List<Order> getOrder = orderRepository.findByRestaurantName(restaurantName);
+		
+		if(!getOrder.isEmpty()) {
+			Order ofOrder = getOrder.get(0);
+			
+			return ofOrder;
+		}
+		
+		return null;
+	}
 	
 	
 	//bottom stuff. work on above when you get chance
@@ -18,5 +38,20 @@ public class OrderService {
 	
 	@Autowired
 	private FanoutExchange fanout;
-
+	
+	public void requestReservation( 
+            String name, 
+            String cuisine, 
+            String price,
+            String items) {
+	String msg  = "{\"Name\": \""+ name + 
+        "\" \"cuisine\": \""+cuisine+
+        "\" \"price\": \""+price+
+        "\" \"items\": \""+items+"\"}" ;
+	System.out.println("Sending message:"+msg);
+	rabbitTemplate.convertSendAndReceive(
+         fanout.getName(), 
+         "",   // routing key none.
+         msg);
+	}
 }
